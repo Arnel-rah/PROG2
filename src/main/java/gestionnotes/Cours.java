8c9df2835ccf927c2c3c5e1866fd3cb6833d1acc
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-11-16 20:22:39 +0300

    feat: add classes Cours

---
+++ src/main/java/gestionnotes/Cours.java
@@ -0,0 +1,15 @@
+package gestionnotes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Cours {
+    private int id;
+    private String label;
+    private int credits;
+    private Enseignants enseignant;
+}
